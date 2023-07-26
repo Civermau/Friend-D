aedb97c48363cf364fd7cb024aefa2004b725bc5
Civer <mausan.bedolla@gmail.com>
2023-07-26 17:49:44 -0600

    Agregar archivos de proyecto.

---
+++ Friend =)/Character.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+public class Character
+{
+    public enum personalities
+    {
+        Extroverted,
+        Introverted,
+        Ambiverted,
+        Optimistic,
+        Pessimistic,
+        Realistic,
+        Assertive,
+        Reserved,
+        Friendly,
+        Shy,
+        Outgoing,
+        Conscientious,
+        Easygoing,
+        Perfectionist,
+        Empathetic,
+        Logical,
+        Creative,
+        Analytical,
+        Adventurous,
+        Humble
+    }
+
+    String personality;
+    int age;
+
+
+    public string setPersonality() {  }
+}
